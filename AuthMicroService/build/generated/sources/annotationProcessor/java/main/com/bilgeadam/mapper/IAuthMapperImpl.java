5d18be584738a3f8e46de8e1e2819e093de3ad5f
OgulcanTekin <ogulcantekin97@gmail.com>
2023-04-17 00:28:47 +0300

    proje

---
+++ AuthMicroService/build/generated/sources/annotationProcessor/java/main/com/bilgeadam/mapper/IAuthMapperImpl.java
@@ -0,0 +1,49 @@
+package com.bilgeadam.mapper;
+
+import com.bilgeadam.dto.request.RegisterRequestDto;
+import com.bilgeadam.dto.request.UserProfileSaveRequestDto;
+import com.bilgeadam.repository.entity.Auth;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-04-16T23:51:32+0300",
+    comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.1.jar, environment: Java 17.0.5 (Amazon.com Inc.)"
+)
+@Component
+public class IAuthMapperImpl implements IAuthMapper {
+
+    @Override
+    public Auth toAuth(RegisterRequestDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Auth.AuthBuilder<?, ?> auth = Auth.builder();
+
+        auth.name( dto.getName() );
+        auth.nameSecond( dto.getNameSecond() );
+        auth.surname( dto.getSurname() );
+        auth.surnameSecond( dto.getSurnameSecond() );
+        auth.email( dto.getEmail() );
+        auth.password( dto.getPassword() );
+        auth.status( dto.getStatus() );
+
+        return auth.build();
+    }
+
+    @Override
+    public UserProfileSaveRequestDto fromAuth(Auth auth) {
+        if ( auth == null ) {
+            return null;
+        }
+
+        UserProfileSaveRequestDto.UserProfileSaveRequestDtoBuilder userProfileSaveRequestDto = UserProfileSaveRequestDto.builder();
+
+        userProfileSaveRequestDto.authid( auth.getId() );
+        userProfileSaveRequestDto.email( auth.getEmail() );
+
+        return userProfileSaveRequestDto.build();
+    }
+}
